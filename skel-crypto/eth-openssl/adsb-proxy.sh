066c17343ab339dc5e29d5c1ecdb858ea744cdd3
Dev0 <dev0@syspulse.io>
2021-08-29 09:33:34 +0300

    Rename for clarity

--- skel-crypto/eth-openssl/adsb-proxy.sh
+++ skel-crypto/eth-openssl/adsb-proxy.sh
@@ -10,6 +10,6 @@ while read data
 do
    #echo "raw: $data"
    ts=`echo $(($(date +%s%N)/1000000))`
-   sig=`echo $data | ./sig-data-sign.sh | ./sig-data-hex.sh`
+   sig=`echo $data | ./sig-data-sign.sh | paste - - -|awk '{print $13,$20}'|awk -F':' '{print $2,$3}'`
    echo "$data $ts $sig"
 done < <(nc -q -1 $RP_HOST $RP_PORT)
