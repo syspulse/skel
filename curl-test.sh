fdd10318fc542f51d31e66373dbfde82773dfb6e
Dev0 <dev0@syspulse.io>
2021-12-20 23:39:19 +0200

    Add CA diagram

--- curl-test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PORT=8083
-
-curl -H "Content-type: application/json" -X POST -d '{"secret": "123456", "name": "app3", "uri": "http://1"}' http://localhost:$PORT/api/v1/otp |jq
-
-curl http://localhost:$PORT/api/v1/otp | jq
-
-#curl http://localhost:$PORT/api/v1/otp/a5cca1c1-2266-47f0-b211
--- skel-crypto/README.md
+++ skel-crypto/README.md
@@ -8,3 +8,11 @@ __NOTE__: for PoC and prototyping
 2. [eth-keystore](eth-keystore) - Ethereum Keystore
 3. [keccak](keccak) - Keccak256 tool
 
+----
+
+## Certificates
+
+### SSL Certificates Architecture
+
+<img src="doc/CA.jpg" width="900">
+
