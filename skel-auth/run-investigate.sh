453b4b92ef16f8ca6a6e35701ff6012a89e1199b
Dev0 <dev0@syspulse.io>
2022-01-09 13:44:48 +0200

    Add investiage start script

---
+++ skel-auth/run-investigate.sh
@@ -0,0 +1,22 @@
+#!/bin/bash                                                                                                                                                                                            
+CWD=`echo $(dirname $(readlink -f $0))`
+cd $CWD
+
+t=`pwd`;
+APP=`basename "$t"`
+CONF=`echo $APP | awk -F"-" '{print $2}'`
+
+export SITE=${SITE:-$CONF}
+
+source auth-cred-${SITE}.sh
+
+MAIN=${MAIN:-io.syspulse.skel.auth.investigate.GoogleOAuth2}
+
+echo "app: $APP"
+echo "site: $SITE"
+echo "main: $MAIN"
+
+echo "AUTH_CLIENT_ID=$AUTH_CLIENT_ID"
+echo "AUTH_CLIENT_SECRET=$AUTH_CLIENT_SECRET"
+
+exec ../run-app.sh $APP $MAIN "$@"
