c837496efccf85319fbd26a493964f09be63c61a
Dev0 <dev0@syspulse.io>
2022-01-18 12:12:04 +0200

    Add test with examples

---
+++ skel-serde/src/test/scala/io/syspulse/skel/serde/MsgPackSerdeSpec.scala
@@ -0,0 +1,63 @@
+package io.syspulse.skel.serde
+
+import org.scalatest.{Ignore}
+import org.scalatest.wordspec.{ AnyWordSpec}
+import org.scalatest.matchers.should.{ Matchers}
+import org.scalatest.flatspec.AnyFlatSpec
+
+import upickle._
+import upickle.default.{ReadWriter => RW, macroRW}
+
+import java.time._
+import io.jvm.uuid._
+//import io.syspulse.skel.util.Util
+
+import scala.jdk.CollectionConverters
+
+
+class MsgPackSerdeSpec extends AnyWordSpec with Matchers {
+  import DataObjMsgPack._
+
+  "MsgPackSerdeSpec" should {
+
+    "should serialize and deserialize String" in {
+      val dd = upack.write(upack.Str("StrData"))
+      dd.size should !== (0)
+      val s = upack.read(dd)
+      s === ("StrData")
+    }
+
+    "should serialize and deserialize DataObj" in {
+      val ts = ZonedDateTime.now()
+      val msg = upickle.default.writeMsg(
+        DataObjMsgPack(UUID("c3ce9adb-8008-426a-8828-6dfdf732df95"),ts.toString,"str",10,Long.MaxValue,"data".getBytes)
+      )
+      val dd = upack.write(msg)
+      dd.size should !== (0)
+      val s = upickle.default.readBinary[DataObjMsgPack](dd)
+      s === (DataObjMsgPack(UUID("c3ce9adb-8008-426a-8828-6dfdf732df95"),ts.toString,"str",10,Long.MaxValue,"data".getBytes))
+    }
+
+    "should serialize and deserialize Lists via Msg" in {
+      val msg = upickle.default.writeMsg(
+        DataList("measure-0",List(Data(10L,DataUnit(1.0,"m/s")),Data(20L,DataUnit(2.0,"kg"))))
+      )
+      val dd = upack.write(msg)
+      dd.size should !== (0)
+      
+      val s = upickle.default.readBinary[DataList](dd)
+      s === (DataList("measure-0",List(Data(10L,DataUnit(1.0,"m/s")),Data(20L,DataUnit(2.0,"kg")))))
+    }
+
+    "should serialize and deserialize Lists via Binary (performance)" in {
+      val dd = upickle.default.writeBinary(
+        DataList("measure-0",List(Data(10L,DataUnit(1.0,"m/s")),Data(20L,DataUnit(2.0,"kg"))))
+      )
+      dd.size should !== (0)
+      
+      val s = upickle.default.readBinary[DataList](dd)
+      s === (DataList("measure-0",List(Data(10L,DataUnit(1.0,"m/s")),Data(20L,DataUnit(2.0,"kg")))))
+    }
+
+  }
+}
