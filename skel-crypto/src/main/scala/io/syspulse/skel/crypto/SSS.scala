89e7a58b6b1cb7467a89dad957c5cfb165c2133a
Dev0 <dev0@syspulse.io>
2022-01-29 16:19:58 +0200

    Add byte array

--- skel-crypto/src/main/scala/io/syspulse/skel/crypto/SSS.scala
+++ skel-crypto/src/main/scala/io/syspulse/skel/crypto/SSS.scala
@@ -14,15 +14,18 @@ import io.syspulse.skel.crypto.key
 
 object SSS {
 
-  def createShares(data:String,requiredShares:Int=3, totalShares:Int=5):Try[List[Share]] = {
+  def createShares(data:String,requiredShares:Int=3, totalShares:Int=5):Try[List[Share]] = 
+    createShares(data.getBytes(),requiredShares,totalShares)  
+  
+  def createShares(data:Array[Byte],requiredShares:Int, totalShares:Int):Try[List[Share]] = {
     SSSS.shares(secret = data, requiredParts = requiredShares, totalParts = totalShares) match {
       case Right(shares) => Success(shares)
       case Left(err) => Failure(new Exception(err.toString))
     }
   }
 
-  def getShares(secretShares:List[Share]):Try[String] = {
-    SSSS.combine(secretShares.take(secretShares.size)) match {
+  def getSecret(secretShares:List[Share]):Try[Array[Byte]] = {
+    SSSS.combineToBytes(secretShares.take(secretShares.size)) match {
       case Right(data) => Success(data)
       case Left(err) => Failure(new Exception(err.toString))
     }
--- skel-crypto/src/test/scala/io/syspulse/skel/crypto/SSSSpec.scala
+++ skel-crypto/src/test/scala/io/syspulse/skel/crypto/SSSSpec.scala
@@ -23,42 +23,42 @@ class SSSSpec extends AnyWordSpec with Matchers with TestData {
     "recover secret from 5 shares of 5" in {
       val sss = SSS.createShares("secret data",3,5)
       sss shouldBe a [Success[_]]
-      val d1 = SSS.getShares(sss.get)
+      val d1 = SSS.getSecret(sss.get)
       d1 === ("secret data")
     }
 
     "recover secret from 3 first shares of 5" in {
       val sss = SSS.createShares("secret data",3,5)
       sss shouldBe a [Success[_]]
-      val d1 = SSS.getShares(sss.get.take(3))
+      val d1 = SSS.getSecret(sss.get.take(3))
       d1 === ("secret data")
     }
 
     "recover secret from 3 last shares of 5" in {
       val sss = SSS.createShares("secret data",3,5)
       sss shouldBe a [Success[_]]
-      val d1 = SSS.getShares(sss.get.drop(2))
+      val d1 = SSS.getSecret(sss.get.drop(2))
       d1 === ("secret data")
     }
 
     "NOT recover secret from 2 shares of 5" in {
       val sss = SSS.createShares("secret data",3,5)
       sss shouldBe a [Success[_]]
-      val d1 = SSS.getShares(sss.get.take(2))
+      val d1 = SSS.getSecret(sss.get.take(2))
       d1 !== ("secret data")
     }
 
     "NOT recover secret from 1 shares of 5" in {
       val sss = SSS.createShares("secret data",3,5)
       sss shouldBe a [Success[_]]
-      val d1 = SSS.getShares(sss.get.take(1))
+      val d1 = SSS.getSecret(sss.get.take(1))
       d1 !== ("secret data")
     }
 
     "NOT recover secret from 0 shares" in {
       val sss = SSS.createShares("secret data",3,5)
       sss shouldBe a [Success[_]]
-      val d1 = SSS.getShares(List())
+      val d1 = SSS.getSecret(List())
       d1 !== ("secret data")
     }
 
