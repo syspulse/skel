b12f8d0d1b7b2bcd64ec0c73351961a60fec467f
Dev0 <dev0@syspulse.io>
2022-05-21 16:25:19 +0300

    Add postgres docker-compose
    [+] Fix old scripts

--- skel-db/postgres/create-db.sh
+++ skel-db/postgres/create-db.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-ROOT_USER=root
-ROOT_PASS=309d5a3c5987f77fc5952f15b20a1818161df57c
-DB_INSTANCE=bip
+ROOT_USER=postgres
+ROOT_PASS=root_pass
 
-DB_USER=shop_user
-DB_PASS=shop_pass
-DB_DATABASE=shop_db
+DB_INSTANCE=test_db
+#DB_DATABASE=test_db
 
-#DB_HOST=localhost
-DB_HOST=mydb1.cpmw9srbc1xl.eu-west-1.rds.amazonaws.com
+DB_USER=test_user
+DB_PASS=test_pass
+
+DB_HOST=localhost
 
 PGPASSWORD=$ROOT_PASS psql --host=$DB_HOST --port=5432 --username=$ROOT_USER --dbname=$DB_INSTANCE -f create-db.sql
