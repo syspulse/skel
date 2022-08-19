6ed8824e32b952f23a06fdf79cc48e470c8efd62
Dev0 <dev0@syspulse.io>
2022-08-19 15:57:32 +0300

    Forgotten files

---
+++ skel-core/src/main/scala/io/syspulse/skel/Ingestable.scala
@@ -0,0 +1,12 @@
+package io.syspulse.skel
+
+import scala.jdk.CollectionConverters._
+
+import io.syspulse.skel
+import io.syspulse.skel.util.Util
+
+trait Ingestable extends Product {
+    def toLog:String = toCSV
+    def toSimpleLog:String = toString()
+    def toCSV:String = Util.toCSV(this)
+}
