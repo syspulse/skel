114f61882e9780344ac2e9a9862bf951a0eab7c4
Dev0 <dev0@syspulse.io>
2022-11-07 00:41:23 +0200

    Update readme

---
+++ demo/env-test.sh
@@ -0,0 +1,8 @@
+export SMTP_HOST=mail.domain.org:465
+export SMTP_USER=user
+export SMTP_PASS=pass
+export SMTP_FROM=no-reply@domain.org
+
+export AWS_ACCOUNT=XXXXXXXXXXX
+
+export NOTIFY_EMAIL=admin@domain.org
