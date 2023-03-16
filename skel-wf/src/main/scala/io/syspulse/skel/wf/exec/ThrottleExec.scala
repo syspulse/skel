c434a5a3665579127370414cc68f20b92942b55b
Dev0 <dev0@syspulse.io>
2023-03-16 17:29:36 +0200

    Add emit commnad to continue flow with Data Event

--- skel-wf/src/main/scala/io/syspulse/skel/wf/exec/ThrottleExec.scala
+++ skel-wf/src/main/scala/io/syspulse/skel/wf/exec/ThrottleExec.scala
@@ -7,9 +7,10 @@ import scala.util.{Try,Success,Failure}
 import io.syspulse.skel.wf.runtime._
 import io.syspulse.skel.wf._
 
-class ThrottleExec(wid:Workflowing.ID,name:String,dataExec:Map[String,Any]) extends Executing(wid,name,dataExec) {
-  val throttle:Long = dataExec.getOrElse("throttle",1000L).toString.toLong
+class ThrottleExec(wid:Workflowing.ID,name:String,dataExec:Map[String,Any]) extends Executing(wid,name,dataExec) {  
   override def exec(in:Let.ID,data:ExecData):Try[ExecEvent] = {    
+    val throttle:Long = data.attr.get("throttle").orElse(dataExec.get("throttle")).getOrElse("1000").toString.toLong
+
     log.info(s"throttling: ${throttle}...")
     
     Thread.sleep(throttle)
