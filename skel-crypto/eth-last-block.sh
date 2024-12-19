dd9dd1dbc50476ced2334c7c1726d85092062b06
Dev0 <dev0@syspulse.io>
2024-12-19 14:19:37 +0200

    Make tools dockerable

--- skel-crypto/eth-last-block.sh
+++ skel-crypto/eth-last-block.sh
@@ -2,7 +2,7 @@
 
 ETH_RPC_URL=${ETH_RPC_URL:-http://localhost:8545}
 
-latest=`curl -s POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' -H "Content-Type: application/json" ${ETH_RPC_URL} |jq -r .result.number | xargs printf "%d"`
+latest=`curl -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' -H "Content-Type: application/json" ${ETH_RPC_URL} |jq -r .result.number | xargs printf "%d"`
 
 echo $latest
 printf "0x%x\n" $latest
