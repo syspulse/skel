aa0b7c7fd3b24c7ab893224ea3c538975e0a6212
Dev0 <dev0@syspulse.io>
2025-03-19 13:49:02 +0200

    Add auto-verison to docker-push

--- tools/docker-push.sh
+++ tools/docker-push.sh
@@ -18,6 +18,8 @@ APP_VER=`echo $APP_FULL | awk -F':' '{print $2}'`
 
 if [ "$VERSION" != "" ]; then
   APP_VER=$VERSION
+else
+  APP_VER=$(basename `ls target/scala-2.13/*.pom | tail -1` .pom | awk -F_ '{print $2}'| awk -F- '{print $2}')
 fi
 
 if [ "$APP_VER" != "" ]; then
