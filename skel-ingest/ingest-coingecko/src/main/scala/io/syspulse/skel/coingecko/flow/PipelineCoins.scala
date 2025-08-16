937e08a3096461874f40cca57c71764c81a2a591
Dev0 <dev0@syspulse.io>
2025-08-16 21:06:57 +0300

    Add Coingecko Pipeline

---
+++ skel-ingest/ingest-coingecko/src/main/scala/io/syspulse/skel/coingecko/flow/PipelineCoins.scala
@@ -0,0 +1,150 @@
+package io.syspulse.skel.coingecko.flow
+
+import scala.annotation.tailrec
+
+import scala.jdk.CollectionConverters._
+import scala.concurrent.duration.{Duration,FiniteDuration}
+import java.util.concurrent.TimeUnit
+import com.typesafe.scalalogging.Logger
+
+import spray.json._
+
+import akka.util.ByteString
+import akka.http.javadsl.Http
+import akka.http.scaladsl.model.HttpRequest
+import akka.http.scaladsl.model.headers.Accept
+import akka.http.scaladsl.model.MediaTypes
+import akka.http.scaladsl
+import akka.stream.scaladsl.Source
+import akka.stream.scaladsl.Flow
+
+import io.syspulse.skel.util.Util
+import io.syspulse.skel.ingest.flow.Pipeline
+import io.syspulse.skel.Ingestable
+
+import io.syspulse.skel.serde.Parq._
+import com.github.mjakubowski84.parquet4s.ParquetRecordEncoder
+import com.github.mjakubowski84.parquet4s.ParquetSchemaResolver
+import akka.actor.ActorSystem
+
+import io.syspulse.skel.blockchain.Coin
+import io.syspulse.skel.blockchain.Token
+
+import io.syspulse.skel.coingecko.Config
+import io.syspulse.skel.coingecko.CoingeckoJson
+import io.syspulse.skel.coingecko.Coingecko
+import io.syspulse.skel.coingecko.{Coingecko_Coin,Coingecko_CoinData}
+import scala.concurrent.Future
+
+object CoinsJson extends DefaultJsonProtocol {
+  implicit val js_Token = jsonFormat6(Token)
+  implicit val js_Coin = jsonFormat7(Coin)
+}
+import CoinsJson._
+
+abstract class PipelineCoingecko[I,T,O <: Ingestable](feed:String,output:String,config:Config)
+  (implicit fmt:JsonFormat[O],parqEncoders:ParquetRecordEncoder[O],parsResolver:ParquetSchemaResolver[O],as:Option[ActorSystem] = None) extends 
+      Pipeline[I,T,O](
+        feed, 
+        output,
+        config.throttle,
+        config.delimiter,
+        config.buffer,
+        throttleSource = config.throttleSource,
+        format = config.format) {
+
+  val log = Logger(s"${this}")  
+  implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
+
+  @volatile
+  var coingecko:Option[Coingecko] = None
+
+  def sid() = "coingecko"
+
+  override def source(feed:String):Source[ByteString,_] = {    
+    feed.split("://").toList match {
+      case ("coingecko" | "cg") :: _ => 
+        val (cg,src) = Coingecko.fromCoingecko(feed)
+        coingecko = Some(cg)
+        src
+      case _ => 
+        super.source(feed)
+    }    
+  }
+}
+
+class PipelineCoins(feed:String,output:String)(implicit config:Config) extends 
+      PipelineCoingecko[String,Coin,Coin](feed, output,config) {
+
+  import CoingeckoJson._
+  
+  // data is a list of array of coin id objects
+  override def parse(data: String): Seq[String] = {
+    if(data.isBlank()) 
+      return Seq.empty
+    
+    val coins = data.parseJson.convertTo[Seq[Coingecko_Coin]]
+    log.info(s"[${sid()}] coins=${coins.size}")
+    coins.map(_.id)
+  }
+
+  // process every coin
+  override def process:Flow[String,Coin,_] = {    
+    Flow[String]
+      .filter( id => {
+        config.filter.isEmpty || config.filter.contains(id)
+      })
+      .mapAsync(1)(id  => {
+        coingecko.get.askCoinsAsync(Set(id))
+        // Future.successful(JsObject(
+        //   "id" -> JsString(id),
+        //   "symbol" -> JsString(id),
+        //   "name" -> JsString(id),
+        // ))
+        .recoverWith {
+          case e: Exception =>
+            // Log the error
+            log.warn(s"Failed: id=${id}: ${e.getMessage}")
+            // Return a failed future to trigger restart
+            akka.pattern.after(FiniteDuration(config.retryDelay,TimeUnit.MILLISECONDS), system.scheduler) {
+              Future.failed(e)
+            }
+        }
+      })      
+      .map(js => js.convertTo[Coingecko_CoinData])
+      .map(c => {
+        
+        val tokens:Map[String,Token] = if(! c.platforms.isDefined) 
+          Map.empty 
+        else {
+          c.platforms.get.flatMap{ case(bid,addr) => 
+            if(addr.isBlank()) 
+              None 
+            else {
+              val dec = c.detail_platforms(bid).decimal_place.getOrElse(18)
+              Some(Token(
+                bid = bid,
+                sym = c.symbol,
+                addr = addr,              
+                dec = dec,              
+              ))
+            }
+          }
+          .map(t => t.bid -> t)
+          .toMap
+        }
+
+        Coin(
+          sym = c.symbol,         
+          tokens = tokens, 
+          icon = Some(c.image.large),
+          sid = Some("cg"),
+          xid = Some(c.id),
+        )
+      })
+  }
+  
+  override def transform(coin: Coin): Seq[Coin] = {    
+    Seq(coin)
+  }
+}
