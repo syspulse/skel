112bc53589f15acbdc4035346625fb91e06b0196
Dev0 <dev0@syspulse.io>
2025-12-27 09:52:51 +0200

    -> 0.18.0 (to align with Sentinel)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -67,7 +67,7 @@ object Dependencies {
     lazy val appNameElastic = "skel-elastic"
     lazy val appBootClassElastic = "io.syspulse.skel.ingest.elastic.App"
 
-    lazy val skelVersion = "0.17.0"
+    lazy val skelVersion = "0.18.0"
     
     lazy val jarPrefix = "server-"
     
