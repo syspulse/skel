4155302fc78b1fd7c38538af9fe3cd64367afadd
Dev0 <dev0@syspulse.io>
2026-05-09 12:28:28 +0300

    Refactor FutureUtil
    [!] Previous commits are broken

---
+++ skel-core/src/main/scala/io/syspulse/skel/FutureUtil.scala
@@ -0,0 +1,52 @@
+package io.syspulse.skel
+
+import scala.util.{Try,Success,Failure}
+import scala.collection.immutable
+
+//import spray.json.DefaultJsonProtocol._
+import com.typesafe.scalalogging.Logger
+
+import scala.concurrent.duration._
+import java.util.concurrent.TimeUnit
+import scala.concurrent.{Await, ExecutionContext, Future}
+
+import akka.NotUsed
+
+import scala.concurrent.Future
+import scala.util.{ Failure, Success }
+
+import io.jvm.uuid._
+
+class FutureAwaitable[T](f:Future[T],timeout:Duration = FutureUtil.timeout0)  {
+  def await[R]() = Await.result(f,timeout)
+}
+
+object FutureUtil {
+  val timeout0 = FiniteDuration(5,TimeUnit.SECONDS)
+  implicit def ftor[R](f: Future[R]):FutureAwaitable[R] = new FutureAwaitable[R](f)
+  
+  implicit def await[R](f: Future[R])(implicit timeout:Duration = Duration.Inf):R = {
+    Await.result(f,timeout)
+  }
+
+  implicit def sync[R](f: Future[R])(implicit timeout:Long = 5000):Try[R] = {
+    Try(Await.result(f,FiniteDuration(timeout,TimeUnit.MILLISECONDS)))
+  }
+}
+
+
+trait AwaitableService[T <: AwaitableService[T]] {
+  var timeout:FiniteDuration = FutureUtil.timeout0
+  
+  def await[R](rsp:Future[R]):R = {
+    val r = Await.result(rsp,timeout)
+    r
+  }
+
+  def withTimeout(timeout:FiniteDuration = FiniteDuration(1000, MILLISECONDS)):T = {
+    this.timeout = timeout
+    // a bit dirty
+    this.asInstanceOf[T]
+  }
+
+}
