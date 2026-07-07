cdad39b66ff33062d911fb2673cabdee189febe5
Dev0 <dev0@syspulse.io>
2026-07-07 13:12:01 +0300

    Add full JVM modules

--- infra/docker/s3/docker-build-openjdk.sh
+++ infra/docker/s3/docker-build-openjdk.sh
@@ -3,8 +3,14 @@ set -euo pipefail
 
 IMAGE=openjdk:21-skel
 IMAGE_COMPAT=openjdk:21-slim
+IMAGE_FULL=openjdk:21-skel-full
+IMAGE_FULL_COMPAT=openjdk:21-slim-full
 
-docker build -f Dockerfile.openjdk -t $IMAGE . "$@"
-docker tag $IMAGE $IMAGE_COMPAT
+docker build -f Dockerfile.openjdk -t "$IMAGE" . "$@"
+docker tag "$IMAGE" "$IMAGE_COMPAT"
 
-docker run --rm $IMAGE_COMPAT java -version
+docker build -f Dockerfile.openjdk.full -t "$IMAGE_FULL" . "$@"
+docker tag "$IMAGE_FULL" "$IMAGE_FULL_COMPAT"
+
+docker run --rm "$IMAGE_COMPAT" java -version
+docker run --rm "$IMAGE_FULL_COMPAT" java -version
