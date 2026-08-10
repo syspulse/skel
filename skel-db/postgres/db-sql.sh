5b7567c4523b3ed4edf15c5ae4f063dff3c93872
Dev0 <dev0@syspulse.io>
2026-08-10 14:02:49 +0300

    Add exprerimental postgres env vars for db scripts

--- skel-db/postgres/db-sql.sh
+++ skel-db/postgres/db-sql.sh
@@ -10,8 +10,11 @@ echo "DB_HOST=$DB_HOST"
 
 SQL_FILE=${1}
 
+# pass the env credentials to the SQL as psql variables (so *.sql never hardcode user/pass/db)
+PSQL_VARS="-v DB_USER=${DB_USER} -v DB_PASS=${DB_PASS} -v DB_DATABASE=${DB_DATABASE}"
+
 if [ "$ALL_DB" != "" ]; then
-PGPASSWORD=$DB_PASS psql --host=$DB_HOST --port=5432 --username=$DB_USER -f ${SQL_FILE}
+PGPASSWORD=$DB_PASS psql --host=$DB_HOST --port=5432 --username=$DB_USER $PSQL_VARS -f ${SQL_FILE}
 else
-PGPASSWORD=$DB_PASS psql --host=$DB_HOST --port=5432 --username=$DB_USER -d $DB_DATABASE -f ${SQL_FILE}
+PGPASSWORD=$DB_PASS psql --host=$DB_HOST --port=5432 --username=$DB_USER -d $DB_DATABASE $PSQL_VARS -f ${SQL_FILE}
 fi
